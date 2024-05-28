b074a0df2e74c81af50215a656454d3d3d26060a
Tesfa Yohannes <tmyohannes@gmail.com>
2024-05-28 20:13:29 +0800

    draft pass

--- dialog.py
+++ dialog.py
@@ -4,23 +4,12 @@
 import gradio as gr
 from openai import OpenAI
 
-
 # gets API Key from environment variable openai .env file
 
 with open(".env") as f:
     api_key = f.read().strip()
 
-
-
 client = OpenAI(api_key=api_key)
-
-# # technical_assistant = client.beta.assistants.create(
-# #     name='asst_j66ZdbapEwQd5WBjHuW7dCqG'
-# # )
-
-
-# # retrieve an assistant from client.beta
-
 def get_assistant(name):
     return client.beta.assistants.retrieve(name)
 
@@ -31,7 +20,6 @@ assistants = {'@tech': 'asst_j66ZdbapEwQd5WBjHuW7dCqG',
               }
 
 fetched_assistants = {key: get_assistant(value) for key, value in assistants.items()}
-
 
 thread = client.beta.threads.create()
 
@@ -81,10 +69,54 @@ def message_hander(message, history):
 
     return result
 
+def clear_status():
+    global thread
+
+    thread = client.beta.threads.create()
+
+    # call chat
+
+
+
+
+
+
     
-    
-demo = gr.ChatInterface(message_hander).queue()
+
+with gr.Blocks(theme='gradio/monochrome') as demo:
+    chat = gr.ChatInterface(fn=message_hander, title='Chat Convo')
+
+    # add button to clear
+    button = gr.Button("Use this instead Clear")
+
+    button.click(clear_status)
+
+    gr.Markdown("""
+                
+                We have definied the following assistants:
+                @tech: A technical assistant that can answer technical questions.
+                @design: A design assistant that can answer design questions.
+                @manager: A manager assistant that can answer managerial questions.
+
+                They could be defined in code or can be found here https://platform.openai.com/playground/assistants?assistant=asst_e46hmiot1roakseFZnw1mByP
+
+                To use them type first `@tech` or `@design` or `@manager` followed by space your question. That is
+
+                ```
+                @design what do you think i should do for fun tv stand. 
+
+                
+                ```
+                ## Memory
+                The memory is shared and managed by the threads api. 
+
+                to clear, clear the chat and also press the 'use this instead clear'
+
+                
+
+                """
+                )
 
 if __name__ == "__main__":
 
-    demo.launch()
+    demo.queue().launch()
